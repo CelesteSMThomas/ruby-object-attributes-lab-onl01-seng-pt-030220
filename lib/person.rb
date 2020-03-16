e380cf73c849671924a8000dd256aeb912946c17
Celeste Thomas <celestesmthomas@gmail.com>
2020-03-16 22:35:32 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name=(person)
+    @name = person
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(occupation)
+    @job = occupation
+  end
+  
+  def job
+    @job
+  end
+    
+end
